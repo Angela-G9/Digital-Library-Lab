4066b77df54858e9819ff2c0d594e099b14e723a
Angela-G9 <angelagathoni9568@gmail.com>
2025-02-02 20:47:45 +0300

    correction

--- phase-2/Repeat-code-challenge/my-react-app/src/Components/BookList.jsx
+++ phase-2/Repeat-code-challenge/my-react-app/src/Components/BookList.jsx
@@ -1,13 +1,32 @@
 import React from "react";
 
-function BookList({ books }) {
+function BookList({ books, deleteBook, setEditingBook, searchTerm }) {
+  // Highlight search term in text
+  const highlightText = (text, term) => {
+    if (!term) return text;
+    const regex = new RegExp(`(${term})`, "gi");
+    return text.split(regex).map((part, index) =>
+      part.toLowerCase() === term.toLowerCase() ? (
+        <span key={index} className="highlight">
+          {part}
+        </span>
+      ) : (
+        part
+      )
+    );
+  };
+
   return (
     <div className="book-list">
       {books.map((book) => (
         <div key={book.id} className="book-item">
-          <h3>{book.title}</h3>
-          <p>Author: {book.author}</p>
-          <p>Genre: {book.genre}</p>
+          <h3>{highlightText(book.title, searchTerm)}</h3>
+          <p>Author: {highlightText(book.author, searchTerm)}</p>
+          <p>Genre: {highlightText(book.genre, searchTerm)}</p>
+          <div className="actions">
+            <button onClick={() => setEditingBook(book)}>Edit</button>
+            <button onClick={() => deleteBook(book.id)}>Delete</button>
+          </div>
         </div>
       ))}
     </div>
